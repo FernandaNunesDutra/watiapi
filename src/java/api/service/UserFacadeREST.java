e21e88a23104979c34a5e02416313b59b8d97fe6
fernanda <fernandanunesdutra96@gmail.com>
2018-09-23 10:57:10 -0300

    api login do usuário

--- src/java/api/service/UserFacadeREST.java
+++ src/java/api/service/UserFacadeREST.java
@@ -12,17 +12,13 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import java.util.List;
 import javax.ejb.Stateless;
-import javax.json.Json;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -42,33 +38,6 @@ public class UserFacadeREST extends AbstractFacade<User> {
         super(User.class);
     }
 
-    @POST
-    @Override
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public void create(User entity) {
-        super.create(entity);
-    }
-
-    @PUT
-    @Path("{id}")
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public void edit(@PathParam("id") Long id, User entity) {
-        super.edit(entity);
-    }
-
-    @DELETE
-    @Path("{id}")
-    public void remove(@PathParam("id") Long id) {
-        super.remove(super.find(id));
-    }
-
-    @GET
-    @Path("{id}")
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public User find(@PathParam("id") Long id) {
-        return super.find(id);
-    }
-
     @GET
     @Override
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
@@ -76,27 +45,13 @@ public class UserFacadeREST extends AbstractFacade<User> {
         return super.findAll();
     }
 
-    @GET
-    @Path("{from}/{to}")
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public List<User> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
-        return super.findRange(new int[]{from, to});
-    }
-
-    @GET
-    @Path("count")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String countREST() {
-        return String.valueOf(super.count());
-    }
-    
     @POST
-    @Path("createUserProfile")
+    @Path("login")
     @Consumes(MediaType.APPLICATION_JSON)
-    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    public Response createUserProfile(String login) {
+    public Response login(String login) {
       
         try{
+            
             JsonParser parser = new JsonParser();
             JsonObject o = parser.parse(login).getAsJsonObject();
             String password = o.get("password").getAsString();
@@ -116,6 +71,7 @@ public class UserFacadeREST extends AbstractFacade<User> {
         }
         
     }
+    
     @Override
     protected EntityManager getEntityManager() {
         return em;
