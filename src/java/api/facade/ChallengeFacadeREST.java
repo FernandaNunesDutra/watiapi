e152e7750794788c18b6a3d4b97456a65426bf34
Unknown <fernandanunesdutra96@gmail.com>
2018-10-07 12:23:28 -0300

    challenge

--- src/java/api/facade/ChallengeFacadeREST.java
+++ src/java/api/facade/ChallengeFacadeREST.java
@@ -10,13 +10,8 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -25,7 +20,7 @@ import javax.ws.rs.core.MediaType;
  * @author fernanda
  */
 @Stateless
-@Path("api.model.challenge")
+@Path("challenge")
 public class ChallengeFacadeREST extends AbstractFacade<Challenge> {
 
     @PersistenceContext(unitName = "watiapiPU")
@@ -35,16 +30,9 @@ public class ChallengeFacadeREST extends AbstractFacade<Challenge> {
         super(Challenge.class);
     }
 
-    @POST
-    @Override
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public void create(Challenge entity) {
-        super.create(entity);
-    }
-
     @GET
     @Override
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces(MediaType.APPLICATION_JSON)
     public List<Challenge> findAll() {
         return super.findAll();
     }
